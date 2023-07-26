39e797e0245f05905fb0f5da7e76d80f06b04f66
Santhosh S <santhoshsekar2903@gmail.com>
2023-07-26 19:39:40 +0530

    Deployment Error Resolve 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ContactUs from "./components/ContactUs"
 import { Header } from "./components/Header"
 import { HeroSection } from "./components/HeroSection"
+import Services from "./components/Services"
 import WhoWeAre from "./components/WhoWeAre"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <HeroSection/>
       <WhoWeAre/>
+      <Services/>
       <ContactUs/>
     </>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import AccountingAuditAssurance from './AccountingAuditAssurance'
-import Advisory from './Advisory'
-import Legal from './Legal'
-import Tax from './Tax'
+import React from "react";
+import AccountingAuditAssurance from "./AccountingAuditAssurance";
+import Advisory from "./Advisory";
+import Legal from "./Legal";
+import Tax from "./Tax";
 
 const Services = () => {
   return (
     <>
-      <div className="our_services section" id='services'>
-            <h1>Our Services</h1>
-            <AccountingAuditAssurance/>
-            <Advisory/>
-            <Legal/>
-            <Tax/>
+      <div className="our_services section" id="services">
+        <h1>Our Services</h1>
+        <AccountingAuditAssurance />
+        <Advisory />
+        <Legal />
+        <Tax />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
